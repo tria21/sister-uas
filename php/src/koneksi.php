329b3fd55cee9bb3db92e5d2f5234f3dd5536eb8
tria21 <sitiamaliafitriani@gmail.com>
2021-12-25 18:41:50 +0700

    ayolaaaaahhhhhhh!!!!!

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -1,14 +1,9 @@
 <?php
-$koneksi = mysqli_connect 
-			(
-				"localhost",
-				"root",
-				"",
-				"crud"
-			);
-if (mysqli_connect_errno())
-	{
-		echo "Koneksi Gagal"
-		.mysqli_connect_error();
-	}
+define('DB_SERVER', '172.20.0.2');
+define('DB_USER', 'root');
+define('DB_PASS', 'admin');
+define('DB_NAME', 'crud');
+
+	$koneksi=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
 ?>
